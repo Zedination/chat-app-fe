2f3607d3e5bb08e5eb999f2ab9be2a4fed9fcc7e
Zedination <50323429+Zedination@users.noreply.github.com>
2022-08-10 00:52:16 +0700

    update src FE

--- src/components/PopupCallVideoOne.vue
+++ src/components/PopupCallVideoOne.vue
@@ -11,7 +11,8 @@ const options = {
     x: "center",
     y: "center",
     width: "60%",
-    height: "70%"
+    height: "70%",
+    index: 10000
 }
 const props = defineProps({
     title: {
@@ -58,9 +59,28 @@ const isVideoEnable = ref(props.videoEnable);
 let waitCall = ref(true);
 const caller = ref(null);
 
+const isShowBottomControl = ref(false);
+const isClickShowBottomControl = ref(false);
+
 const onCloseWindow = (force) => {
     stopStream();
     emit("onClose");
+}
+
+const onResizeWindow = event => {
+    if (event.width <= 630) {
+        isShowBottomControl.value = isClickShowBottomControl.value = true;
+    } else {
+        isShowBottomControl.value = isClickShowBottomControl.value = false;
+    }
+}
+
+const toggleAudio = () => {
+    isAudioEnable.value = !isAudioEnable.value;
+}
+
+const toggleVideo = () => {
+    isVideoEnable.value = !isVideoEnable.value;
 }
 async function initStream() {
     stopStream();
@@ -187,6 +207,8 @@ onMounted(async () => {
     // peer.value.on('open', function (id) {
     //     console.log('My peer ID is: ' + id);
     // });
+    
+    if (isMobile()) winboxRef.value.winbox.fullscreen();
     await initStream();
     initSelectedDevice();
     if (props.isCaller) startCall();
@@ -199,11 +221,22 @@ onMounted(async () => {
         })
     }
 })
+/**
+ * Check device is mobile by user agent
+ */
+function isMobile() {
+    var check = false;
+    (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
+    return check;
+}
+const toggleBottomControl  = () => {
+    isClickShowBottomControl.value = !isClickShowBottomControl.value;
+}
 </script>
 <template>
-    <VueWinBox ref="winboxRef" :options="options" @onclose="onCloseWindow($event)">
-        <div class="container-fluid content-area" style="height: 100%;">
-            <div class="header-video row">
+    <VueWinBox ref="winboxRef" :options="options" @onclose="onCloseWindow($event)" @onresize="onResizeWindow($event)">
+        <div class="container-fluid content-area" style="height: 100%;" @click="toggleBottomControl">
+            <div v-show="!isShowBottomControl" class="header-video row">
                 <div class="col-2 d-flex justify-content-start align-items-center">
                     <div><i class="fas fa-dot-circle" style="color: red;"></i> 00:10:15</div>
                 </div>
@@ -219,15 +252,19 @@ onMounted(async () => {
                         </button>
                     </div>
                     <div class="p-1 border-start border-light">
-                        <button type="button" class="button-icon">
-                            <i v-if="isVideoEnable" class="fas fa-video" style="color: white;"></i>
-                            <i v-else class="fas fa-video-slash" style="color: white;"></i>
+                        <button v-if="isVideoEnable" type="button" class="button-icon" @click="toggleVideo">
+                            <i class="fas fa-video" style="color: white;"></i>
+                        </button>
+                        <button v-else type="button" class="button-icon" @click="toggleVideo">
+                            <i class="fas fa-video-slash" style="color: white;"></i>
                         </button>
                     </div>
                     <div class="p-1">
-                        <button type="button" class="button-icon">
-                            <i v-if="isAudioEnable" class="fas fa-microphone" style="color: white;"></i>
-                            <i v-else class="fas fa-microphone-slash" style="color: white;"></i>
+                        <button v-if="isAudioEnable" type="button" class="button-icon" @click="toggleAudio">
+                            <i class="fas fa-microphone" style="color: white;"></i>
+                        </button>
+                        <button v-else type="button" class="button-icon" @click="toggleAudio">
+                            <i class="fas fa-microphone-slash" style="color: white;"></i>
                         </button>
                     </div>
                     <div class="p-1">
@@ -239,7 +276,7 @@ onMounted(async () => {
                     </div>
                 </div>
             </div>
-            <div class="body-video row" style="padding: 0 !important; overflow: hidden;">
+            <div class="body-video row" :class="{'body-video-mobile': isShowBottomControl}" style="padding: 0 !important; overflow: hidden;">
                 <div class="stream-area" id="stream-area">
                     <video id="remoteVideo" ref="remoteVideoRef" class="remote-video video-container" autoplay
                         playsinline></video>
@@ -253,27 +290,31 @@ onMounted(async () => {
                 <div class="sidebar-area" id="sidebar-area">
                     <a href="javascript:void(0)" class="closebtn" @click="closeSideBar">X</a>
                 </div>
-                <div class="mobile-control row">
-                    <div class="col-12 d-flex align-items-center justify-content-end">
-                        <div class="p-1">
-                            <button type="button" class="button-icon" @click="showSideBar">
+                <div :class="{'mobile-control': isShowBottomControl && isClickShowBottomControl}" style="display: none;">
+                    <div class="d-flex align-items-center justify-content-center">
+                        <div style="display: inline-block;">
+                            <button type="button" class="button-icon">
                                 <i class="fas fa-ellipsis-h" style="color: white;"></i>
                             </button>
                         </div>
-                        <div class="p-1 border-start border-light">
-                            <button type="button" class="button-icon">
-                                <i v-if="isVideoEnable" class="fas fa-video" style="color: white;"></i>
-                                <i v-else class="fas fa-video-slash" style="color: white;"></i>
+                        <div style="display: inline-block;">
+                            <button v-if="isVideoEnable" type="button" class="button-icon" @click.stop="toggleVideo">
+                                <i class="fas fa-video" style="color: white;"></i>
+                            </button>
+                            <button v-else type="button" class="button-icon" @click.stop="toggleVideo">
+                                <i class="fas fa-video-slash" style="color: white;"></i>
                             </button>
                         </div>
-                        <div class="p-1">
-                            <button type="button" class="button-icon">
-                                <i v-if="isAudioEnable" class="fas fa-microphone" style="color: white;"></i>
-                                <i v-else class="fas fa-microphone-slash" style="color: white;"></i>
+                        <div style="display: inline-block;">
+                            <button v-if="isAudioEnable" type="button" class="button-icon" @click.stop="toggleAudio">
+                                <i class="fas fa-microphone" style="color: white;"></i>
+                            </button>
+                            <button v-else type="button" class="button-icon" @click.stop="toggleAudio">
+                                <i class="fas fa-microphone-slash" style="color: white;"></i>
                             </button>
                         </div>
-                        <div class="p-1">
-                            <button @click="onCloseWindow('')" type="button" class="btn btn-danger"><i
+                        <div style="display: inline-block;">
+                            <button @click.stop="onCloseWindow('')" type="button" class="btn btn-danger"><i
                                     class="fas fa-phone fa-rotate-90"></i></button>
                         </div>
                     </div>
@@ -288,9 +329,9 @@ onMounted(async () => {
     container-type: inline-size;
     container-name: winbox;
 }
-.winbox {
+/* .winbox {
     z-index: 10000 !important;
-}
+} */
 
 .wb-header {
     background-color: #16a085 !important;
@@ -298,6 +339,8 @@ onMounted(async () => {
 }
 </style>
 <style scoped>
+
+
 
 
 /* @import '../../node_modules/winbox/dist/css/winbox.min.css'; */
@@ -439,9 +482,26 @@ onMounted(async () => {
 }
 
 .mobile-control {
-    display: none;
+    background-color: rgb(61, 61, 61);
+    border-radius: 5px;
+    display: block !important;
+    position: absolute;
+    bottom: 90px;
+    margin: auto;
+    height: 48.5px;
+    width: 193px;
+    left: calc((100% - 193px)/2);
+    z-index: 10015;
+    padding-left: 4px !important;
+    padding-right: 4px !important;
 }
-@media only screen and (max-width: 630px) {
+
+.body-video-mobile {
+    height: 100% !important;
+    position: relative;
+}
+
+/* @media only screen and (max-width: 630px) {
     .mobile-control {
         background-color: rgb(61, 61, 61);
         border-radius: 5px;
@@ -449,8 +509,12 @@ onMounted(async () => {
         position: absolute;
         bottom: 90px;
         margin: auto;
-        height: 50px;
+        height: 48.5px;
+        width: 193px;
+        left: calc((100% - 193px)/2);
         z-index: 10015;
+        padding-left: 4px !important;
+        padding-right: 4px !important;
     }
 
     .body-video {
@@ -461,5 +525,5 @@ onMounted(async () => {
     .header-video {
         display: none;
     }
-}
+} */
 </style>
